aee20659d67193b1cb8c9c0c1efc4d5bc3ddd23f
jessicaavg <jessica.vegaa@students.amikom.ac.id>
2022-04-06 22:25:47 +0700

    commit pertama

---
+++ LatihanMethod/LatihanMethod/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Calculator
+    {
+        //membuat method
+        public int Penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        //overload method
+        public int Penjumlahan(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        //method static
+        public static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ LatihanOverLoadingMethod/LatihanOverLoadingMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverLoadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+
+            Console.WriteLine("Minimum #1 : {0}", number.FindMinimum(30, 41));
+            Console.WriteLine("Minimum #2 : {0}", number.FindMinimum(63, 52, 74));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1 : {0}", number.FindMaximum(53, 31));
+            Console.WriteLine("Maximum #2 : {0}", number.FindMaximum(83, 22, 94));
+
+            Console.ReadKey();
+        }
+    }
+}
